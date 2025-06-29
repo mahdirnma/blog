d10babf4b3530c8f4f4baf51e41ee5f4df961fc0
mahdirnma <rahnamamahdi78@gmail.com>
2025-06-29 20:35:04 +0330

    Initial commit

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'title' => 'camera',
+            'description' => 'lorem ipsum',
+        ]);
+        Category::create([
+            'title' => 'car',
+            'description' => 'lorem ipsum2',
+        ]);
+    }
+}
